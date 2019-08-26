805f3d4ea108df561d539b8e23742362b43e8770
sunzhi <sunzhi.im@bytedance.com>
2019-08-26 23:14:05 +0800

    [Fix] client flag parser demo

--- internal/app/cli/config/config.go
+++ internal/app/cli/config/config.go
@@ -5,20 +5,25 @@ import (
 	"fmt"
 	"time"
 
+	_ "github.com/golang/go/src/pkg/log"
 	"github.com/inhzus/go-redis-impl/internal/pkg/client"
 )
 
+func logInit() {
+	_ = flag.Set("stderrthreshold", "INFO")
+}
+
 func Init() *client.Option {
+	logInit()
 	option := &client.Option{}
 	var host, port string
 	var readTimeout, writeTimeout int64
 	flag.StringVar(&option.Proto, "protocol", "tcp", "protocol")
 	flag.StringVar(&host, "host", "127.0.0.1", "host")
 	flag.StringVar(&port, "port", "6389", "port")
-	flag.Int64Var(&readTimeout, "read timeout", 0, "read timeout (ms), \"0\" represents unlimited")
-	flag.Int64Var(&writeTimeout, "write timeout", 0, "write timeout (ms), \"0\" represents unlimited")
+	flag.Int64Var(&readTimeout, "rt", 0, "read timeout (ms), \"0\" represents unlimited")
+	flag.Int64Var(&writeTimeout, "wt", 0, "write timeout (ms), \"0\" represents unlimited")
 	flag.Parse()
-	_ = flag.Set("stderrthreshold", "INFO")
 	option.Addr = fmt.Sprintf("%s:%s", host, port)
 	option.ReadTimeout = time.Duration(readTimeout) * time.Millisecond
 	option.WriteTimeout = time.Duration(writeTimeout) * time.Millisecond
--- internal/app/cli/main.go
+++ internal/app/cli/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
 	"github.com/golang/glog"
 	"github.com/inhzus/go-redis-impl/internal/app/cli/config"
 	"github.com/inhzus/go-redis-impl/internal/pkg/client"
@@ -9,8 +12,15 @@ import (
 func main() {
 	conf := config.Init()
 	cli := client.NewClient(conf)
-	glog.Infof("config: %s", conf)
 	_ = cli.Connect()
+	//cli.Close()
+	glog.Infof("config: %s", conf)
+	pipe := cli.Pipeline()
+	for i := 0; i < 20; i++ {
+		pipe.Set(fmt.Sprintf("a%d", i), i, time.Hour)
+	}
+	_, _ = pipe.Exec()
+	cli.Set("a", 1, 0)
 	cli.Get("s")
 	_ = cli
 }
